89cc1b6f14b945ccbe87557f0fb8d56bb500623b
MONKEYFACE678 <144086481+MONKEYFACE678@users.noreply.github.com>
2024-07-15 21:19:55 -0600

    big push bc i foprgor

---
+++ Programming-Theory-Repo/Assets/Scripts/ProjectileAI/ProjectileMain.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileMain : MonoBehaviour
+{
+    [SerializeField] protected int speed;
+    [SerializeField] protected GameObject target;
+    [SerializeField] protected int damage;
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+
+    public void GetTargetandDam(GameObject target, int dam)
+    {
+        this.target = target;
+        damage = dam;
+    }
+
+    private void Damage()
+    {
+        target.GetComponent<IDamageable>().AdjustHealth(-damage);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == target)
+        {
+            Damage();
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Programming-Theory-Repo/Assets/Scripts/ProjectileAI/SkeleRangedProjectileAI.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkeleRangedProjectileAI : ProjectileMain
+{
+    private void Awake()
+    {
+        speed = 30;
+    }
+}
